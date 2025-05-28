bb828da72f171fa232e4c97f0fe6f4ca30c6a988
michal.dzierzbicki <michal.dzierzbicki@thehutgroup.com>
2025-05-28 04:57:51 +0200

    Move dependency into application and fix excludes

--- thumbnail/Adapter/Symfony/Application/DependencyInjection/DoctrineMappingPass.php
+++ thumbnail/Adapter/Symfony/Application/DependencyInjection/DoctrineMappingPass.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Dullahan\Thumbnail\Adapter\Symfony\DependencyInjection;
+namespace Dullahan\Thumbnail\Adapter\Symfony\Application\DependencyInjection;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -17,7 +17,7 @@ class DoctrineMappingPass implements CompilerPassInterface
         }
 
         $namespace = 'Dullahan\\Thumbnail\\Domain\\Entity';
-        $path = realpath(dirname(__FILE__) . '/../../../Domain/Entity');
+        $path = realpath(dirname(__FILE__) . '/../../../../Domain/Entity');
         $mappingDriver = DoctrineOrmMappingsPass::createAttributeMappingDriver(
             [$namespace],
             [$path],
