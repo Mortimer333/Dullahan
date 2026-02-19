40fbb8e049e048a02bee49824cfda4e6a416fc4b
Michał Dzierzbicki <michal.dzierzbicki@vp.pl>
2026-02-19 18:06:05 +0100

    Jackrabbit asset headers allowing for proper browser caching

--- asset/Application/Server/BigAssetServer.php
+++ asset/Application/Server/BigAssetServer.php
@@ -13,10 +13,7 @@ class BigAssetServer implements AssetServerInterface
     {
         header('Content-Description: File Transfer');
         header('Content-Type: ' . $asset->mimeType);
-        header('Content-Disposition: attachment; filename="' . $asset->name . '.' . $asset->extension . '"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
+        header('Cache-Control: public, max-age=31536000, immutable');
         header('Content-Length: ' . $asset->weight);
 
         $file = $asset->getResource();
