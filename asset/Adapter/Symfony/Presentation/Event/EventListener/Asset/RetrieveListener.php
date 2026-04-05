95068a8e5897f2ea0ac4ab2e183ddddfe7c65c15
Michał Dzierzbicki <michal.dzierzbicki@vp.pl>
2026-04-05 16:38:14 +0200

    HOTFIX: Do not throw during event when asset not found

--- asset/Adapter/Symfony/Presentation/Event/EventListener/Asset/RetrieveListener.php
+++ asset/Adapter/Symfony/Presentation/Event/EventListener/Asset/RetrieveListener.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dullahan\Asset\Adapter\Symfony\Presentation\Event\EventListener\Asset;
 
+use Dullahan\Asset\Domain\Exception\AssetEntityNotFoundException;
+use Dullahan\Asset\Domain\Exception\AssetNotFoundException;
 use Dullahan\Asset\Port\Infrastructure\AssetFileManagerInterface;
 use Dullahan\Asset\Port\Infrastructure\AssetPersistenceManagerInterface;
 use Dullahan\Asset\Presentation\Event\Transport\Retrieve\RetrieveByIdEvent;
@@ -21,16 +23,28 @@ final class RetrieveListener
     #[AsEventListener(event: RetrieveByPathEvent::class)]
     public function onRetrieveByPath(RetrieveByPathEvent $event): void
     {
-        $path = $event->getPath();
-        $event->setEntity($this->assetPersistenceManager->getByPath($path));
-        $event->setStructure($this->assetFileManager->get($path));
+        try {
+            $path = $event->getPath();
+            $event->setEntity($this->assetPersistenceManager->getByPath($path));
+            $event->setStructure($this->assetFileManager->get($path));
+        } catch (AssetEntityNotFoundException|AssetNotFoundException) {
+            $event->setEntity(null);
+            $event->setStructure(null);
+            // Do nothing
+        }
     }
 
     #[AsEventListener(event: RetrieveByIdEvent::class)]
     public function onRetrieveById(RetrieveByIdEvent $event): void
     {
-        $entity = $this->assetPersistenceManager->get((int) $event->getId());
-        $event->setEntity($entity);
-        $event->setStructure($this->assetFileManager->get($entity->getFullPath()));
+        try {
+            $entity = $this->assetPersistenceManager->get((int) $event->getId());
+            $event->setEntity($entity);
+            $event->setStructure($this->assetFileManager->get($entity->getFullPath()));
+        } catch (AssetEntityNotFoundException|AssetNotFoundException) {
+            $event->setEntity(null);
+            $event->setStructure(null);
+            // Do nothing
+        }
     }
 }
